cee918e02c7dfc339d6af9e5af649f6bc522beb2
Richard Negron <richard_257@hotmail.com>
2019-10-03 12:06:04 -0500

    Se agregaron mensajes dde validación dde código

--- js/source.js
+++ js/source.js
@@ -1,10 +1,13 @@
 angular
     .module('app', ['ngMessages'])
-    .directive('customValidator', function ($http, $q) {
+    .directive('customValidator', function ($compile, $http, $q) {
         return {
             restrict: 'A',
-            require: 'ngModel',
-            link: function (scope, element, attr, ngModel) {
+            require: ['ngModel', '^form'],
+            link: function (scope, element, attrs, ctrls) {
+                var ngModel = ctrls[0];
+                var ngForm = ctrls[1];
+
                 ngModel.$validators.required = function (modelValue, viewValue) {
                     var valModel = modelValue || viewValue;
                     return (valModel !== "" && valModel != undefined && valModel !== null && valModel != "null");
@@ -39,6 +42,18 @@ angular
                     return deferred.promise;
                 }
 
+                var nameMessage = ngForm.$name + '.' + attrs.name + '.$error';
+                var eMessages = "<div class='g-error' ng-messages='" + nameMessage + "'>";
+
+                eMessages += "<p ng-message='required'>Este campo es requerido</p>";
+                eMessages += "<p ng-message='pattern'>Debe escribir sólo letras</p>";
+                eMessages += "<p ng-message='patternMinLength'>Debe escribir más de 2 letras</p>";
+                eMessages += "<p ng-message='patternMaxLength'>Debe escribir menos de 10 letras</p>";
+                eMessages += "<p ng-message='black'>Lista negra</p>";
+                eMessages += "</div>"
+
+                var content = $compile(eMessages)(scope);
+                element.after(content);
             }
         }
     });
